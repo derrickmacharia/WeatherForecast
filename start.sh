404cf2c2f61b5d0a9002a43cab8843ccceec785d
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-11-16 10:30:51 +0300

    change email address

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY=Developer
 
-export MAIL_USERNAME=pitchesapi@gmail.com
-export MAIL_PASSWORD=derrick123.
+export MAIL_USERNAME=newweatherforecast@gmail.com
+export MAIL_PASSWORD=moringa123.
 
 python3.8 manage.py server
